d6ac2cb4392d131ebc1e9c8460a54a06a4210bfa
SOURAV PAL <mr.souravjanta@gmail.com>
2024-11-20 15:53:15 +0530

    Palindrome.java

---
+++ Palindrome.java
@@ -0,0 +1,20 @@
+
+public class PalindromeChecker {
+    public static void main(String[] args) {
+        String str = "madam";
+        if (isPalindrome(str)) {
+            System.out.println(str + " is a palindrome.");
+        } else {
+            System.out.println(str + " is not a palindrome.");
+        }
+    }
+    public static boolean isPalindrome(String str) {
+        int n = str.length();
+        for (int i = 0; i < n / 2; i++) {
+            if (str.charAt(i) != str.charAt(n - i - 1)) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
